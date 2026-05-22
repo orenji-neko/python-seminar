06b1fe9488bb1e5f68c1b956834426eef36b51e7
orenji_neko <markanthony.enfermo@outlook.com>
2026-05-22 12:31:25 +0800

    add dockerfile

--- flask-app-client/components/Providers.tsx
+++ flask-app-client/components/Providers.tsx
@@ -1,13 +1,16 @@
 "use client";
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { Toaster } from "@/components/ui/sonner";
+import { useState } from "react";
 
 export default function Providers({ children }: { children: React.ReactNode }) {
-    const queryClient = new QueryClient();
+    const [queryClient] = useState(() => new QueryClient());
 
     return (
         <QueryClientProvider client={queryClient}>
             {children}
+            <Toaster position="top-right" richColors />
         </QueryClientProvider>
     );
 }
